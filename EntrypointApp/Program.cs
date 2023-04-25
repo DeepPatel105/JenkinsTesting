6d16cb8218b9bdc924367feb2590365001f2279a
DeepPatel105 <83176935+DeepPatel105@users.noreply.github.com>
2023-04-25 17:17:27 +0530

    Entrypoint App Project added

---
+++ EntrypointApp/Program.cs
@@ -0,0 +1,19 @@
+﻿
+while (true)
+{
+    Console.Write($"Current Date and Time is {DateTime.Now}.");
+    WriteElipse(.5, 20);
+}
+
+
+static void WriteElipse(double intervalInSeconds, int timesToLoop)
+{
+    int totalTimes = 0;
+    for (int i = 0; i < timesToLoop; i++)
+    {
+        Thread.Sleep(Convert.ToInt32(intervalInSeconds * 1000));
+        Console.Write(".");
+        totalTimes++;
+    }
+    Console.WriteLine();
+}
